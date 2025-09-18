681519e09bbb1a2f724b81031e7e16ffc6cfabcc
Devon Wilkins <devonwlkns@gmail.com>
2025-09-18 08:00:04 -0400

    chore: first React app setup; cleaned starter page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      <h1>Devon’s Beat Store Motha Fucka!</h1>
+      <p>Welcome! This is a fresh React app.</p>
+    </div>
+  );
+}
+
+export default App;
